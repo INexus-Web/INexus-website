414418cb6aedafc35e7f1e4f1d109eac21bde228
Namiki-Niwa <109219684+Namiki-Niwa@users.noreply.github.com>
2024-05-16 00:21:42 +0900

    add top button

--- js/index.js
+++ js/index.js
@@ -7,3 +7,51 @@ images.forEach(function(svg) {
 })
 document.querySelector('main').style.marginTop = navHeight + 'px';
 //
+
+
+// Back to the top
+// $(function(){
+
+//     $("#js-topBtn").hide();
+//     $(window).on("scroll", function(){
+//     if($(this).scrollTop() > 100) {
+//       $("#js-topBtn").fadeIn(600);
+//     } else {
+//       $("#js-topBtn").fadeOut(600);
+//     }
+//   });
+  
+//     $("#js-topBtn").on("click", function(){
+//       $("html, body").animate({ scrollTop:0}, 500);
+//     });
+  
+//   })
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var topBtn = document.getElementById("js-topBtn");
+    topBtn.style.display = "none";
+    topBtn.style.transition = "opacity 0.6s";
+  
+    window.addEventListener("scroll", function() {
+      if (window.pageYOffset > 2000) {
+        topBtn.style.display = "block";
+        setTimeout(function() {
+          topBtn.style.opacity = "1";
+        }, 10); // ボタンが表示される前にopacityを設定
+      } else {
+        topBtn.style.opacity = "0";
+        setTimeout(function() {
+          topBtn.style.display = "none";
+        }, 600); // opacityが0になった後にdisplayをnoneに設定
+      }
+    });
+  
+    topBtn.addEventListener("click", function() {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    });
+  });
+  
